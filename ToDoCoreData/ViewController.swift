eba93aa7159b8547722ea01d5f92cf70e137917b
Elfana, A | Elfaael <elfaael12@gmail.com>
2023-08-20 20:23:10 +0700

    Initial Commit

---
+++ ToDoCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoCoreData
+//
+//  Created by Elfana Anamta Chatya on 20/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
