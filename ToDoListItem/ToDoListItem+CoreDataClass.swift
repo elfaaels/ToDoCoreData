40bf61573d5c172c85ac78611997b40dca41d505
Elfana, A | Elfaael <elfaael12@gmail.com>
2023-08-20 21:18:04 +0700

    feat: initial commit

---
+++ ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoCoreData
+//
+//  Created by Elfana Anamta Chatya on 20/08/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
